16f3f0d38b9f52b4e8b11e51f629f56f524dae6d
hyeokangel <dltkdgur7171@gmail.com>
2021-12-10 14:04:32 +0900

    리액트 강의

--- react-nodebird/prepare/front/components/AppLayout.js
+++ react-nodebird/prepare/front/components/AppLayout.js
@@ -1,10 +1,38 @@
 import PropTypes from "prop-types"
+import Link from "next/link"
+import {Menu, Input, Row, Col} from "antd"
 
 const AppLayout = ({ children }) => {
     return (
         <div>
-            <div>공통메뉴!</div>
-            {children}
+            <Menu mode="horizontal">
+                <Menu.Item>
+                <Link href="/"><a>노드버드</a></Link>
+                </Menu.Item>
+                <Menu.Item>
+                <Link href="/profile"><a>프로필</a></Link>
+                </Menu.Item>
+                <Menu.Item>
+                <Input.Search enterButton style={{verticalAlign: "middle"}}/>
+                </Menu.Item>
+                <Menu.Item>
+                <Link href="/signup"><a>회원가입</a></Link>
+                </Menu.Item>
+            </Menu>
+            <Row gutter={8}>
+                <Col xs={24} md={6}>
+                    왼쪽메뉴
+                </Col>    
+                <Col xs={24} md={12}>
+                {children}
+                </Col>
+                <Col xs={24} md={6}>
+                    <a href="https://github.com/hyeokangel" target="_blank" rel="noreferrer noopener">git hub</a>
+                    {/* target: _blank === 새창으로 띄어라
+                    rel="noreferrer noopener" === 보안위협 사라짐?? */}
+                </Col>    
+            </Row>
+            
         </div>
     )
 };
--- react-nodebird/prepare/front/pages/profile.js
+++ react-nodebird/prepare/front/pages/profile.js
@@ -1,5 +1,16 @@
+import AppLayout from "../components/AppLayout"
+import Head from "next/head"
+
 const Profile = () => {
-    return <div>프로필 페이지</div>
+    return (
+        <>
+        <Head>
+        <title>내프로필 | NodeBird</title>
+        </Head>
+
+    <AppLayout>프로필 페이지</AppLayout>
+    </>
+    )
 }
 
 export default Profile
